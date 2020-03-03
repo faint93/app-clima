94ffdd4ea6f4584327ff6615fc7ec421ea86716d
Ignacio Zuñiga <ignaciozuniga22@gmail.com>
2020-03-02 23:25:41 -0300

    1.0

---
+++ precache-manifest.580af0883be478ecd0f1030e3778575b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b3239193a171bbb64c9afd460b7146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7269817d528ab0ab6239",
+    "url": "/static/css/main.e2040362.chunk.css"
+  },
+  {
+    "revision": "e486f6460558bbe2913a",
+    "url": "/static/js/2.c0d5992c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c0d5992c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7269817d528ab0ab6239",
+    "url": "/static/js/main.9d61e288.chunk.js"
+  },
+  {
+    "revision": "2ffcd7fdf3694ef19d98",
+    "url": "/static/js/runtime-main.e216103f.js"
+  },
+  {
+    "revision": "6012341799d8e33ed42002a54e5ba15e",
+    "url": "/static/media/background.60123417.png"
+  }
+]);
